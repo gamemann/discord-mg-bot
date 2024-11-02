888177c858d75c89cbe8717805751e8e5143cb36
Christian Deacon <christianmdeacon@gmail.com>
2024-11-02 03:54:11 -0400

    Start working on server module.

---
+++ src/server/__init__.py
@@ -0,0 +1,4 @@
+__title__ = "Servers"
+__version__ = "1.0.0"
+
+from .base import Server
---
+++ src/server/base.py
@@ -0,0 +1,8 @@
+class Server():
+    id: str
+    current_game: str
+    
+    def __init__(self, id: str):
+        self.id = id
+    
+        
