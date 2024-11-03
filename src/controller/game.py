91c4b0a84cc196910c5c8b10a35cb103c0cc104e
Christian Deacon <christianmdeacon@gmail.com>
2024-11-03 04:40:26 -0500

    Make progress with game controller.

--- src/controller/game.py
+++ src/controller/game.py
@@ -1,27 +1,43 @@
 import discord
-import threading
-import time
+import asyncio
 
 from datetime import datetime
-
+from bot import Discord, discord_chan
 from server import Server
+from config import Config
+from utils import debug_msg
+
+class Game():
+    def __init__(self, obj: any):
+        self.obj = obj
 
 class GameController():
-    def __init__(self):
-        self.servers = dict[int, Server]
+    def __init__(self, bot: Discord, cfg: Config, servers: dict[int, Server]):
+        self.bot = bot
+        self.cfg = cfg
+        self.servers: dict[int, Server] = servers
         
         self.game_check_time = 30.0
         
-    def game_thread(self):
+        # Subscribe to message channel.
+        discord_chan.subscribe(self.process_msg)
+        
+    async def game_thread(self):
+        debug_msg(1, self.cfg, "Starting game controller thread...")
+        
         while True:
+            debug_msg(4, self.cfg, "Checking servers in game controller thread...")
+            
             # Get currentr date time.
             now = datetime.now()
             
             # Loop through all servers and check
             for k, srv in self.servers.items():
                 # If we aren't ready for a new game, ignore.
-                if srv.cur_game is not None or srv.next_game_cooldown < 1 and now < (srv.last_game + srv.next_game_cooldown):
+                if srv.cur_game is not None or srv.next_game_cooldown < 1 and (srv.last_game is not None and now < (srv.last_game + srv.next_game_cooldown)):
                     continue
+                
+                debug_msg(4, self.cfg, f"Found server #{k} ready for a new game...")
                 
                 try:
                     # Start new game.
@@ -31,7 +47,7 @@ class GameController():
                     print(e)
                 
             # Sleep.
-            time.sleep(self.game_check_time)
+            await asyncio.sleep(self.game_check_time)
         
     def process_msg(self, msg: discord.Message):
         # Make sure this is from within a server.
@@ -41,8 +57,10 @@ class GameController():
         # Extract server ID
         sid = msg.guild.id
         
-        # Make sure we exist in servers.
         if sid not in self.servers:
-            self.servers[sid] = Server()
+            return
             
         srv = self.servers[sid]
+                
+        if srv.cur_game is not None:
+            srv.cur_game.process_msg(msg)
