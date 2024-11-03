11c202a7d528677b16a2e1108deac78b61f06d7b
Christian Deacon <christianmdeacon@gmail.com>
2024-11-03 04:40:11 -0500

    Make progress on server class.

--- src/server/base.py
+++ src/server/base.py
@@ -1,15 +1,34 @@
 import random
+import importlib
 
 from datetime import datetime
+from bot import Discord
 
 class Server():    
-    def __init__(self, id: str):
+    def __init__(self, bot: Discord, id: int, games: dict[str, any]):
+        self.bot = bot
         self.id = id
         
         self.games: dict[str, any] = {}
         
-        self.cur_game: str = None
-        self.last_game = datetime.now()
+        for k, v in games.items():
+            settings = v
+            
+            # We'll want to load the custom game class.
+            # To Do: FIND A BETTER WAY TO DO THIS WITHOUT IMPORTING THE GAME MODULE FOR EVERY SERVER'S GAME.
+            try:
+                m = importlib.import_module(f"game.{k}")
+                game_cl = m.Game(bot = self.bot, **settings)
+            except Exception as e:
+                print(f"Failed to load game '{k}' for server '{self.id}' due to exception.")
+                print(e)
+                
+                continue
+            
+            self.games[k] = game_cl
+        
+        self.cur_game = None
+        self.last_game: datetime = None
         self.next_game_cooldown = 120.0
         
         self.next_game_random = True
@@ -37,13 +56,7 @@ class Server():
     def start_new_game(self):
         # Get next game and start.
         next_game = self.get_next_game_key()
-
-            
-                
         
+        self.cur_game = self.games[next_game]
         
-        
-        
-        
-    
-        
+        self.cur_game.start()
