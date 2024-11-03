8b370882496912a9274785ec1d2de2c0d4c21631
Christian Deacon <christianmdeacon@gmail.com>
2024-11-03 00:31:34 -0400

    Make progress on main script.

--- src/main.py
+++ src/main.py
@@ -1,11 +1,38 @@
 import sys
+# import traceback
 
+from bot import Discord
 from config import Config
+from controller import GameController
+
+HELP_MENU = f"""USAGE: python3 src/main.py [--cfg=<path> -l -h]
+\t--cfg => The path to the config file.
+\t-l --list => Whether to list config contents.
+\t-h --help => Whether to print the help menu."""
 
 def main():
     cfg_path = "./conf.json"
+    list = False
+    help = False
     
-    # To Do: Parse CLI.
+    # Parse CLI.
+    for arg in sys.argv:
+        # Handle config path.
+        if arg.startswith("cfg="):
+            cfg_path = arg.split('=')[1]
+        
+        # Handle list.
+        if arg.startswith("-l") or arg.startswith("--list"):
+            list = True
+            
+        # Handle help menu.
+        if arg.startswith("-h") or arg.startswith("--help"):
+            help = True
+            
+    if help:
+        print(HELP_MENU)
+        
+        sys.exit(0)
     
     # Load config.
     cfg = Config()
@@ -16,9 +43,24 @@ def main():
         print("Failed to load config!")
         print(e)
         
-        sys.exit(1)
+        # traceback.print_exc()
         
-    cfg.print()
+        sys.exit(1)
+    
+    # Check if we want to print the config.
+    if list:
+        cfg.print()
+        
+        sys.exit(0)
+        
+    # We need to configure the game controller before passing to Discord bot.
+    controller = GameController()
+    
+    # Create Discord bot.
+    bot = Discord(cfg.bot.token, controller)
+    
+    # Connect and run bot.
+    bot.connect()
 
 if __name__ == "__main__":
     main()
