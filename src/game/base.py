192a7e64673c04431f38329b0e1b72893fcb9b96
Christian Deacon <christianmdeacon@gmail.com>
2024-11-03 00:30:37 -0400

    Start working on questionnaire game.

--- src/game/base.py
+++ src/game/base.py
@@ -1,25 +1,28 @@
-from bot import Discord    
+import discord
+import random
+
+from bot import Discord
 
 class GameBase():
-    bot: Discord = None
-    name: str = None
-    
-    pick_weight: float = 50.0
-    
     def __init__(self,
         bot: Discord,
         name: str,
-        pick_weight: float = 50.0             
+        pick_weight = 50.0
     ):
         self.bot = bot
         self.name = name
         self.pick_weight = pick_weight
         
-        super().__init__()
+    def start(self):
+        # Pick random question.
+        question = random.choice(self.questions)
         
-    def start():
-        pass
+        self.cur_question_idx = question
     
-    def end():
+    def end(self):
+        # Remove question.
+        self.cur_question = None
+
+    def process_msg(self, msg: discord.Message):
         pass
     
--- src/game/questionnaire.py
+++ src/game/questionnaire.py
@@ -1,8 +1,68 @@
+import discord
+
+from bot import Discord
 from .base import GameBase
 
+class Answer():
+    def __init__(self, answer: str, case_sensitive: bool = False):
+        self.answer = answer
+        self.case_sensitive = case_sensitive
+
+class Question():
+    def __init__(self, question: str, answers: list[Answer], points: int = 1):
+        self.question = question
+        self.answers = answers
+        self.points = points
+
 class Game(GameBase):
-    def start():
-        pass
+    cur_question: Question = None
     
-    def end():
-        pass
+    def __init__(self,
+        bot: Discord,
+        name: str,
+        pick_weight: float,
+        questions: list[Question]            
+    ):
+        self.bot = bot
+        self.name = name
+        self.pick_weight = pick_weight
+        self.questions = questions
+        
+        super().__init__()
+    
+    def start(self):
+        # Execute base class.
+        super().start()
+    
+    def end(self):
+        # Execute base class.
+        super().end()
+    
+    def is_correct(self, input: str):        
+        if self.cur_question is None:
+            return False
+        
+        # Loop through answers
+        for answer in self.cur_question.answers:
+            # Strip input and answer.
+            input_f = input.strip()
+            answer_f = answer.answer.strip()
+            
+            # Check if we should lower-case.
+            if not answer.case_sensitive:
+                input_f = input.lower()
+                answer_f = answer.answer.lower()
+                
+            # Check input and answer.
+            if input_f == answer_f:
+                return True
+        
+        return False
+    
+    def process_msg(self, msg: discord.Message):
+        if self.cur_question is None:
+            return
+        
+        # Check if our content's is correct to the current question.
+        if self.is_correct(msg.content):
+            print("Correct answer!")        
